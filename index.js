91b43745eb1c6f6bc674b542946c4b2945801782
Adryan Corcione <63524187+adryancore@users.noreply.github.com>
2024-05-14 16:53:08 -0400

    added cat api

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const projectSection = document.querySelector("#projects");
-console.log(projectSection);
-const projectsList = projectSection.querySelector("ul");
-console.log(projectsList);
+const catContainer = document.getElementById("catContainer");
 
-fetch("https://api.github.com/users/adryancore/repos").then((res) => {
-    console.log(res);
-    if (!res.ok) {
-        throw new Error("Something went wrong");
+console.log(catContainer);
+
+fetch("https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_I8aBnXYvGao92mK7EDF65eLqXMaLdVxLTWk3vnsdZVSlbWGeLE3XadfDyIZHtJa4")
+    .then((res) => {
+        if (!res.ok) {
+        throw new Error("oh no! invalid");
     }
-    return res.json();
-}).then((data) => {
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-        const project = document.createElement("li");
-        project.innerText = data[i].name;
-        projectsList.appendChild(project);
-    }
+        return res.json();
 })
-.catch((error) => {
-    const errorElement = document.createElement("p");
-    errorElement.innerText = error.message;
-    projectSelection.appendChild(errorElement);
-});
+    .then ((data) => {
+        console.log(data);
+        const catImgUrl = data[0].url;
+        const catImg = document.createElement("img");
+        catImgUrl.src = catImgUrl;
+     
 
-// tiny change here
+        console.log(data[0].breeds[0].name);
+        const breedName  = data[0].breeds[0].name;
+        const catBreedTitle = document.createElement("h2");
+        catBreedTitle.innerText = breedName;
+
+        const breedDescription = document.createElement("p");
+        breedDescription.innerText =  data[0].breeds[0].description;
+
+        catContainer.appendChild(catBreedTitle);
+        catContainer.appendChild(breedDescription);
+        catContainer.appendChild(catImg);
+})
+.catch((err) => {
+    console.warn(err);
+});
