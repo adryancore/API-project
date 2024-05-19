f421d2bb9d8dc5a1d1ebd881df5f4f8981534906
Adryan Corcione <63524187+adryancore@users.noreply.github.com>
2024-05-19 14:35:03 -0400

    updated api project w/ css and more

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fetchCatButton.addEventListener("click", () => {
     }
         return res.json();
 })
-    .then ((data) => {
+    .then((data) => {
         console.log(data);
         const catImgUrl = data[0].url;
         const catImg = document.createElement("img");
@@ -30,8 +30,41 @@ fetchCatButton.addEventListener("click", () => {
         catContainer.appendChild(breedDescription);
         catContainer.appendChild(catImg);
 })
+
 .catch((err) => {
     console.warn(err);
-});
+    });
 });
 
+// const dogContainer = document.getElementById("dogContainer");
+// const fetchDogButton = document.getElementById("fetchDogButton");
+
+// fetchDogButton.addEventListener("click", () => {
+
+//     dogContainer.innerHTML = "";
+
+//     fetch("https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=live_Q95ErQP72gUGM47PC45Cx7OEz6BEPyiIz4WnQOHNwyxVCoUQ971RomybV9ut6651")
+//     .then((res) => {
+//         if (!res.ok) {
+//         throw new Error("oh no! invalid");
+// }
+//         return res.json();
+// })
+//     .then((data) => {
+//         console.log(data);
+//         const dogImgUrl = data[0].url;
+//         const dogImg = document.createElement("img");
+//         dogImg.src = dogImgUrl;
+
+//         console.log(data[0].breeds[0].name);
+//         const breedName  = data[0].breeds[0].name;
+//         const dogBreedTitle = document.createElement("h2");
+//         dogBreedTitle.innerText = breedName;
+
+//         const breedDescription = document.createElement("p");
+//         breedDescription.innerText =  data[0].breeds[0].description;
+
+//         dogContainer.appendChild(dogBreedTitle);
+//         dogContainer.appendChild(breedDescription);
+//         dogContainer.appendChild(dogImg);
+// })
